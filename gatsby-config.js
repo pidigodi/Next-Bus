b888db5d44b8146f3d20031fbfde4326d0ff276b
John Bohl <johnbohl@outlook.com>
2025-10-17 18:58:01 +1300

    first

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,8 +9,8 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Compare & Book with NextBus`,
+    description: `Find reliable charter bus hire in New Zealand. Post your trip once and get quotes from trusted operators for events, tours, schools, and more.`,
     author: `@gatsbyjs`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
